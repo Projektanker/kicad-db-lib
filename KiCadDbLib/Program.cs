e92ec7b118511d0f84cfe788170a66afeaf62d81
Sebastian Rumohr <sebastian.rumohr@outlook.de>
2018-12-30 19:50:47 +0100

    Add project files.

---
+++ KiCadDbLib/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using KiCadDbLib.ViewModels;
+using KiCadDbLib.Views;
+
+namespace KiCadDbLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
