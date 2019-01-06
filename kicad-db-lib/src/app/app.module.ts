58e38ffef2a0584a54f2f2a4cd29bbd3020ea0cb
Sebastian Rumohr <sebastian.rumohr@outlook.de>
2019-01-06 16:36:02 +0100

    refactoring

--- kicad-db-lib/src/app/app.module.ts
+++ kicad-db-lib/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms"; // < -- NgModel lives here
-import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms'; // < -- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
