ff1074bbc01c01a5e433673b30701dd7d594821f
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2019-02-13 13:36:12 +0100

    table colum order changed

--- angular/src/app/part/part.ts
+++ angular/src/app/part/part.ts
@@ -1,29 +1,29 @@
 export class Part {
   id: number = 0;
 
+  // Library the part have be stored in. <see cref="Value"/> must be unique per library.
+  library: string = '';
+
   // Schematic reference. E.g. "R" for resistors
-  reference: string = "";
+  reference: string = '';
 
   // Part value. E.g. "10K". Must be unique per <see cref="Library"/>
-  value: string = "";
-
-  // Footprint of part. Have to match KiCad footprint libraries. E.g. "Resistor_SMD:R_0603_1608Metric"
-  footprint: string = "";
+  value: string = '';
 
   // Reference to symbol. ../lib1.lib:symbol1
-  symbol: string = "";
+  symbol: string = '';
 
-  // Library the part have be stored in. <see cref="Value"/> must be unique per library.
-  library: string = "";
-
-  // Datasheet link
-  datasheet: string = "";
+  // Footprint of part. Have to match KiCad footprint libraries. E.g. "Resistor_SMD:R_0603_1608Metric"
+  footprint: string = '';
 
   // Description of part.
-  description: string = "";
+  description: string = '';
+
+  // Datasheet link
+  datasheet: string = '';
 
   // Keywords to find part
-  keywords: string = "";
+  keywords: string = '';
 
   // Custom fields like manufacturer etc.
   customFields: { [name: string]: string } = {};
