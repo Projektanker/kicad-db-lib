919567df2888175532082276d6ec95747b499dc8
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2020-04-07 18:12:30 +0200

    New dotnet application

---
+++ src/netcore/KiCadDbLib.Cgi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KiCadDbLib.Cgi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
