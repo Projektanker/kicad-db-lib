f208c556c05b3484d45b740aa98277b555b20aa3
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2020-07-30 14:24:36 +0200

    New menu

--- src/netcore/KiCadDbLib/KiCadDbLib/Program.cs
+++ src/netcore/KiCadDbLib/KiCadDbLib/Program.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Reflection;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Icons.FontAwesome;
+﻿using Avalonia;
 using Avalonia.Logging.Serilog;
 using Avalonia.ReactiveUI;
 using KiCadDbLib.ViewModels;
 using KiCadDbLib.Views;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 using ReactiveUI;
 using Splat;
 
 namespace KiCadDbLib
 {
-    class Program
+    internal class Program
     {
+        
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         public static void Main(string[] args)
         {
-            
-
             BuildAvaloniaApp()
                 .StartWithClassicDesktopLifetime(args);
         }
@@ -28,18 +25,21 @@ namespace KiCadDbLib
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
         {
-            FontAwesomeIconProvider.Register();
-
-            // Router uses Splat.Locator to resolve views for
-            // view models, so we need to register our views.
-            //
+            // Router uses Splat.Locator to resolve views for view models, so we need to register
+            // our views.
             Locator.CurrentMutable.Register(() => new PartsView(), typeof(IViewFor<PartsViewModel>));
             Locator.CurrentMutable.Register(() => new SettingsView(), typeof(IViewFor<SettingsViewModel>));
-            
+
             return AppBuilder.Configure<App>()
-                           .UseReactiveUI()
-                           .UsePlatformDetect()
-                           .LogToDebug();
+                .AfterSetup(AfterSetupCallback)       
+                .UseReactiveUI()
+                .UsePlatformDetect()
+                .LogToDebug();
+        }
+
+        private static void AfterSetupCallback(AppBuilder appBuilder)
+        {
+            IconProvider.Register<FontAwesomeIconProvider>();
         }
     }
 }
