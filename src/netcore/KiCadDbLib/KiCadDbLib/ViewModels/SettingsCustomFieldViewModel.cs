8b261d731311700fa053b3e26c4749e48a78f5b0
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2020-08-12 23:24:18 +0200

    Settings

--- src/netcore/KiCadDbLib/KiCadDbLib/ViewModels/SettingsCustomFieldViewModel.cs
+++ src/netcore/KiCadDbLib/KiCadDbLib/ViewModels/SettingsCustomFieldViewModel.cs
@@ -7,62 +7,28 @@ using ReactiveUI;
 
 namespace KiCadDbLib.ViewModels
 {
-    public sealed class SettingsCustomFieldViewModel : ViewModelBase, IEquatable<SettingsCustomFieldViewModel>
+    public sealed class SettingsCustomFieldViewModel : ViewModelBase
     {
-        private readonly ICollection<SettingsCustomFieldViewModel> _parent;
-        private string _value;
+        private readonly Action<string> _removeAction;
 
-        public SettingsCustomFieldViewModel(ICollection<SettingsCustomFieldViewModel> parent, string value)
+        public SettingsCustomFieldViewModel(string value, Action<string> removeAction)
         {
-            _parent = parent ?? throw new ArgumentNullException(nameof(parent));
-            _value = value;
-            IObservable<bool> canAdd = this.WhenAnyValue(
-                vm => vm.Value,
-                changedValue =>
-                {
-                    return !string.IsNullOrWhiteSpace(changedValue)
-                        && !_parent.Contains(this);
-                });
-
-            Add = ReactiveCommand.Create(execute: ExectuteAdd, canExecute: canAdd);
-            Remove = ReactiveCommand.Create(execute: ExectuteRemove);
+            Value = value;
+            _removeAction = removeAction ?? throw new ArgumentNullException(nameof(removeAction));
+            Remove = ReactiveCommand.Create(execute: ExecuteRemove);
         }
-
-        public ReactiveCommand<Unit, Unit> Add { get; }
 
         public ReactiveCommand<Unit, Unit> Remove { get; }
 
         public string Value
         {
-            get => _value;
-            set => this.RaiseAndSetIfChanged(ref _value, value);
+            get;
         }
 
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as SettingsCustomFieldViewModel);
-        }
 
-        public bool Equals(SettingsCustomFieldViewModel other)
+        private void ExecuteRemove()
         {
-            return other != null &&
-                   _value == other._value;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(_value);
-        }
-
-        private void ExectuteAdd()
-        {
-            _parent.Add(new SettingsCustomFieldViewModel(_parent, Value));
-            Value = string.Empty;
-        }
-
-        private void ExectuteRemove()
-        {
-            _parent.Remove(this);
+            _removeAction(Value);
         }
     }
 }
--- src/netcore/KiCadDbLib/KiCadDbLib/ViewModels/SettingsViewModel.cs
+++ src/netcore/KiCadDbLib/KiCadDbLib/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using Avalonia.Rendering.SceneGraph;
+using DynamicData;
 using KiCadDbLib.Models;
 using KiCadDbLib.Services;
 using ReactiveUI;
@@ -17,20 +22,41 @@ namespace KiCadDbLib.ViewModels
     {
         private readonly SettingsService _settingsService;
         ObservableAsPropertyHelper<ObservableCollection<SettingsCustomFieldViewModel>> _customFieldsProperty;
-        ObservableAsPropertyHelper<SettingsCustomFieldViewModel> _newCustomFieldProperty;
         ObservableAsPropertyHelper<Settings> _settingsProperty;
-      
+        private string _newCustomField;
+
         public SettingsViewModel(IScreen hostScreen, SettingsService settingsService)
             : base(hostScreen)
         {
             _settingsService = settingsService;
+
+            var canAddCustomField = this.WhenAnyValue(vm => vm.NewCustomField, value =>
+            {
+                return !string.IsNullOrWhiteSpace(value) 
+                    && !CustomFields.Any(vm => vm.Value.Equals(value, StringComparison.CurrentCulture));
+            });
+
+            AddCustomField = ReactiveCommand.Create(execute: ExecuteAddCustomField, canExecute: canAddCustomField);
+        }
+
+
+        public string NewCustomField
+        {
+            get => _newCustomField;
+            set => this.RaiseAndSetIfChanged(ref _newCustomField, value);
+        }
+
+        private void ExecuteAddCustomField()
+        {
+            CustomFields.Add(new SettingsCustomFieldViewModel(NewCustomField, RemoveCustomField));
+            NewCustomField = string.Empty;
         }
 
         public ObservableCollection<SettingsCustomFieldViewModel> CustomFields => _customFieldsProperty?.Value;
 
         public ReactiveCommand<Unit, Unit> GoBack => HostScreen.Router.NavigateBack;
+        public ReactiveCommand<Unit, Unit> AddCustomField { get; }
 
-        public SettingsCustomFieldViewModel NewCustomField => _newCustomFieldProperty?.Value;
 
         public Settings Settings => _settingsProperty?.Value;
 
@@ -60,24 +86,19 @@ namespace KiCadDbLib.ViewModels
                 .Select(settings => settings.CustomFields)
                 .Select(customFields =>
                 {
-                    var customFieldsCollection = new ObservableCollection<SettingsCustomFieldViewModel>();
-                    foreach (var field in customFields)
-                    {
-                        customFieldsCollection.Add(new SettingsCustomFieldViewModel(customFieldsCollection, field));
-                    }
-
-                    return customFieldsCollection;
+                    IEnumerable<SettingsCustomFieldViewModel> collection =
+                        customFields.Select(cf => new SettingsCustomFieldViewModel(cf, RemoveCustomField));
+                    return new ObservableCollection<SettingsCustomFieldViewModel>(collection);
                 })
-                .ToProperty(this, vm => vm.CustomFields)
+                .ToProperty(this, nameof(CustomFields))
                 .DisposeWith(disposables);
 
-            _newCustomFieldProperty = this.WhenAnyValue(vm => vm.CustomFields)
-                .Where(x => x != null)
-                .Select(cusomFields => new SettingsCustomFieldViewModel(cusomFields, null))
-                .ToProperty(this, vm => vm.NewCustomField)
-                .DisposeWith(disposables);
+        }
 
-
+        private void RemoveCustomField(string customField)
+        {
+            SettingsCustomFieldViewModel vm = CustomFields.Single(vm => vm.Value.Equals(customField, StringComparison.CurrentCulture));
+            CustomFields.Remove(vm);
         }
     }
 }
