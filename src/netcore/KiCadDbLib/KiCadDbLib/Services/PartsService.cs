dd2eed6448c511928200211c93484884c14c17aa
Sebastian Rumohr <sebastian.rumohr@projektanker.de>
2020-08-13 08:54:22 +0200

    Add PartService

---
+++ src/netcore/KiCadDbLib/KiCadDbLib/Services/PartsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KiCadDbLib.Models;
+using Newtonsoft.Json;
+
+namespace KiCadDbLib.Services
+{
+    public class PartsService
+    {
+        private readonly SettingsService _settingsService;
+
+        public PartsService(SettingsService settingsService)
+        {
+            _settingsService = settingsService ?? throw new ArgumentNullException(nameof(settingsService));
+        }
+
+        public async Task<Part[]> GetPartsAsync()
+        {
+            var settings = await _settingsService.GetSettingsAsync();
+
+            JsonSerializer serializer = JsonSerializer.Create();
+            IEnumerable<Task<Part>> tasks = Directory.EnumerateFiles(settings.DatabasePath)
+                .Select(file => Task.Run(() =>
+                {
+                    using StreamReader fileReader = File.OpenText(file);
+                    using JsonTextReader jsonReader = new JsonTextReader(fileReader);
+                    Part part = serializer.Deserialize<Part>(jsonReader);
+                    return part;
+                }));
+
+            Part[] parts = await Task.WhenAll(tasks);
+            return parts;
+        }
+    }
+}
